988dd4afab5bdfe06a22491be0d005f2516398d2
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 14:31:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,38 @@
 class Artist
-  attr_accessor :name
+  @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
+    @songs = []
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def slef.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find do |artist|
+      artist.save
+    end
+  end
+
+  def save 
+    @@all << self 
+  end
+
+  def print_songs
+    songs.each do |song|
+      puts song.name
+    end
+  end
+      
 end
