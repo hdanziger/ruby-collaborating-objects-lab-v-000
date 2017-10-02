5b1a2c0640c95b212523ec87bd964c779091b1d2
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 14:09:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,17 +4,4 @@ class Song
   def initialize(name)
     @name = name
   end
-
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
-
-  def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-  end
-end
+end 
