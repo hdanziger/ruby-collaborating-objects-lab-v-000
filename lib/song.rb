5e20928e0ae77bcad114aa264371c8902974bb56
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 14:33:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = song.new.split(" - ")
+    artist, song = song.new.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
