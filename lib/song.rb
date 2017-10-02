008debbb39cbfd587fd83c801710ed454cb2a8c7
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 13:55:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,15 @@ class Song
   attr_accessor :title
 
   def initialize
-    @name = name 
+    @name = name
+  end
+
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
   end
 
   def self.new_by_filename(filename)
